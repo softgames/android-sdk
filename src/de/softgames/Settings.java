ec15f062ef9a06be295dac15754f39496a825707
Roland Castillo <rolandaugusto@gmail.com>
2013-02-11 17:01:56 +0100

    Initial commit

---
+++ src/de/softgames/Settings.java
@@ -0,0 +1,12 @@
+package de.softgames;
+
+public final class Settings {
+	
+	static final String SERVER_URL = "http://mobile-notifications.softgames.de/devices";
+
+	/**
+	 * Google API project id registered to use GCM.
+	 */
+	static final String SENDER_ID = "1079642456342";
+
+}
