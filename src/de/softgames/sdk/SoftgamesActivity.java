5f913c65d94f0b14079161b53b004d63561204b3
Roland Castillo <rolandaugusto@gmail.com>
2013-05-11 16:15:33 +0200

    Keep screen awake

--- src/de/softgames/sdk/SoftgamesActivity.java
+++ src/de/softgames/sdk/SoftgamesActivity.java
@@ -4,7 +4,6 @@
  */
 package de.softgames.sdk;
 
-
 import java.util.Locale;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -41,7 +40,6 @@ import de.softgames.sdk.util.NetworkType;
 import de.softgames.sdk.util.NetworkUtilities;
 import de.softgames.sdk.util.SGSettings;
 
-
 /**
  * The Class SoftgamesIntro.
  * 
@@ -71,7 +69,7 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
     private OpenxAdView loadingScreenAdView;
 
     private OpenxAdView crossPromoAdView;
-    
+
     private ImageView teaserImage;
 
     private Button buttonPlay;
@@ -97,75 +95,78 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	super.onCreate(savedInstanceState);
 
-        // We want to show the splash screen and the ads in full screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	// We want to show the splash screen and the ads in full screen
+	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Initializes the GA tracker object
-        initGoogleAnalytics();
+	// Keep screen awake
+	getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        // Let's initialize the ad related objects
-        initOpenxAds();
+	// Initializes the GA tracker object
+	initGoogleAnalytics();
 
-        setContentView(R.layout.sg_flipper);
-        res = getResources();
-        flipper = (ViewFlipper) findViewById(R.id.softgames_master);
-        mTracker.sendView("/SplasScreen");
-        
-        // The Openx ads are instantiated
-        loadingScreenAdView = (OpenxAdView) findViewById(R.id.adview);
-        crossPromoAdView = (OpenxAdView) findViewById(R.id.adview_xpromo);
+	// Let's initialize the ad related objects
+	initOpenxAds();
 
-        buttonPlay = (Button) findViewById(R.id.button_play);
+	setContentView(R.layout.sg_flipper);
+	res = getResources();
+	flipper = (ViewFlipper) findViewById(R.id.softgames_master);
+	mTracker.sendView("/SplasScreen");
 
-        // Custom type face
-        TextView xpromoDividerText = (TextView) findViewById(R.id.divider_text);        
-        TextView teaserGameName = (TextView) findViewById(R.id.teaser_text);
-        if (SGSettings.gameName != null && SGSettings.gameName != "") {
-            teaserGameName.setText(SGSettings.gameName);
-        }
-        try {
-            Typeface typeface = Typeface.createFromAsset(getAssets(),
-                    "oswald.ttf");
-            xpromoDividerText.setTypeface(typeface);
-            teaserGameName.setTypeface(typeface, 1);
-        } catch (Exception e) {
-            Log.e(TAG, "The font oswald_bold is missing from the assets folder");
-        }
-        
-        teaserImage = (ImageView) findViewById(R.id.teaserImage);
-        if (SGSettings.getTeaserImage() != null) {
-            teaserImage.setImageDrawable(SGSettings.teaserImage);
-        } else {
+	// The Openx ads are instantiated
+	loadingScreenAdView = (OpenxAdView) findViewById(R.id.adview);
+	crossPromoAdView = (OpenxAdView) findViewById(R.id.adview_xpromo);
 
-        }
-        
-        scheduleTaskExecutor = Executors.newScheduledThreadPool(POOL_SIZE);
-        // Thread to display a splash screen during the given seconds
-        scheduleTaskExecutor.schedule(new Runnable() {
+	buttonPlay = (Button) findViewById(R.id.button_play);
 
-            @Override
-            public void run() {
+	// Custom type face
+	TextView xpromoDividerText = (TextView) findViewById(R.id.divider_text);
+	TextView teaserGameName = (TextView) findViewById(R.id.teaser_text);
+	if (SGSettings.gameName != null && SGSettings.gameName != "") {
+	    teaserGameName.setText(SGSettings.gameName);
+	}
+	try {
+	    Typeface typeface = Typeface.createFromAsset(getAssets(),
+		    "oswald.ttf");
+	    xpromoDividerText.setTypeface(typeface);
+	    teaserGameName.setTypeface(typeface, 1);
+	} catch (Exception e) {
+	    Log.e(TAG, "The font oswald_bold is missing from the assets folder");
+	}
 
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (isFirstSession()) {
-                            showLoadingScreen();
-                        } else {
-                            // crossPromoAdView = (OpenxAdView)
-                            // findViewById(R.id.adview_xpromo);
-                            showCrosspromotion();
-                        }
+	teaserImage = (ImageView) findViewById(R.id.teaserImage);
+	if (SGSettings.getTeaserImage() != null) {
+	    teaserImage.setImageDrawable(SGSettings.teaserImage);
+	} else {
 
-                    }
-                });
-            }
-        }, SGSettings.SPLASH_DELAY, TimeUnit.SECONDS);
+	}
 
-        buttonPlay.setOnClickListener(this);
+	scheduleTaskExecutor = Executors.newScheduledThreadPool(POOL_SIZE);
+	// Thread to display a splash screen during the given seconds
+	scheduleTaskExecutor.schedule(new Runnable() {
+
+	    @Override
+	    public void run() {
+
+		runOnUiThread(new Runnable() {
+		    @Override
+		    public void run() {
+			if (isFirstSession()) {
+			    showLoadingScreen();
+			} else {
+			    // crossPromoAdView = (OpenxAdView)
+			    // findViewById(R.id.adview_xpromo);
+			    showCrosspromotion();
+			}
+
+		    }
+		});
+	    }
+	}, SGSettings.SPLASH_DELAY, TimeUnit.SECONDS);
+
+	buttonPlay.setOnClickListener(this);
 
     }
 
@@ -173,76 +174,76 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      * Inits the google analytics.
      */
     private void initGoogleAnalytics() {
-        // Get the GoogleAnalytics singleton.
-        mGaInstance = GoogleAnalytics.getInstance(this);
+	// Get the GoogleAnalytics singleton.
+	mGaInstance = GoogleAnalytics.getInstance(this);
 
-        // Use the GoogleAnalytics singleton to get a Tracker.
-        mTracker = mGaInstance.getTracker("UA-39037923-1");
+	// Use the GoogleAnalytics singleton to get a Tracker.
+	mTracker = mGaInstance.getTracker("UA-39037923-1");
     }
 
     /**
      * initializes the necessary objects to display ads.
      */
     private void initOpenxAds() {
-	 String language = "", countryCode = "";
-        // Gets an instance of window manager for display related tasks
-        WindowManager windowManager = getWindowManager();
-        // The density is gather in order to determine the pixel ratio
-        Float density = SoftgamesUI.getScreenDensity(windowManager);
+	String language = "", countryCode = "";
+	// Gets an instance of window manager for display related tasks
+	WindowManager windowManager = getWindowManager();
+	// The density is gather in order to determine the pixel ratio
+	Float density = SoftgamesUI.getScreenDensity(windowManager);
 
-        String packageName = getApplicationContext().getPackageName();
-        Display display = windowManager.getDefaultDisplay();
-        try {
-            Locale locale = getResources().getConfiguration().locale;        
-            TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-            countryCode = tm.getSimCountryIso();
-            if (countryCode == null || countryCode.equals("")) {
-    	    countryCode = locale.getCountry();
-    	}
-            language = locale.getDisplayLanguage();
+	String packageName = getApplicationContext().getPackageName();
+	Display display = windowManager.getDefaultDisplay();
+	try {
+	    Locale locale = getResources().getConfiguration().locale;
+	    TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+	    countryCode = tm.getSimCountryIso();
+	    if (countryCode == null || countryCode.equals("")) {
+		countryCode = locale.getCountry();
+	    }
+	    language = locale.getDisplayLanguage();
 	} catch (Exception e) {
-	    Log.e(TAG, "There was an error getting the language and country code");
-	}        
-        
-        int connectionType = NetworkUtilities.getConnectionType(getApplicationContext());
-        SoftgamesAd softgamesAd = new SoftgamesAd(packageName,
-                display.getWidth(), display.getHeight(), density,
-                connectionType,
-                Build.MANUFACTURER, language, countryCode);
-        Log.d(TAG, softgamesAd.toString());
-        OpenxAdView.setSoftgamesAd(softgamesAd);
-        
-        String sInternetStatus = "no";
-        if (connectionType != NetworkType.UNKNOWN.getValue()) {
-           sInternetStatus = "yes";
-	} 
-        mTracker.sendEvent("internet_connection", sInternetStatus, "type", Long.valueOf(connectionType));
-    }
+	    Log.e(TAG,
+		    "There was an error getting the language and country code");
+	}
 
-    
+	int connectionType = NetworkUtilities
+		.getConnectionType(getApplicationContext());
+	SoftgamesAd softgamesAd = new SoftgamesAd(packageName,
+		display.getWidth(), display.getHeight(), density,
+		connectionType, Build.MANUFACTURER, language, countryCode);
+	Log.d(TAG, softgamesAd.toString());
+	OpenxAdView.setSoftgamesAd(softgamesAd);
+
+	String sInternetStatus = "no";
+	if (connectionType != NetworkType.UNKNOWN.getValue()) {
+	    sInternetStatus = "yes";
+	}
+	mTracker.sendEvent("internet_connection", sInternetStatus, "type",
+		Long.valueOf(connectionType));
+    }
 
     /**
      * Shows the screen with the cross promotion from openx.
      */
     private void showCrosspromotion() {
 
-        if (!NetworkUtilities.isOnline(this)) {
-            if (SGSettings.isInternetRequired()) {
-                buildRetryConnectionDialog();
-            } else {
-                startApp();
-            }
-        } else {
-            try {
-                crossPromoAdView.load();
-                // flipper.setInAnimation(SoftgamesUI.inFromRightAnimation());
-                flipper.setDisplayedChild(XPROMO_SCREEN_ID);
-                mTracker.sendView("/CrossPromotionPage");
+	if (!NetworkUtilities.isOnline(this)) {
+	    if (SGSettings.isInternetRequired()) {
+		buildRetryConnectionDialog();
+	    } else {
+		startApp();
+	    }
+	} else {
+	    try {
+		crossPromoAdView.load();
+		// flipper.setInAnimation(SoftgamesUI.inFromRightAnimation());
+		flipper.setDisplayedChild(XPROMO_SCREEN_ID);
+		mTracker.sendView("/CrossPromotionPage");
 
-            } catch (Exception e) {
-                Log.e(TAG, "error", e);
-            }
-        }
+	    } catch (Exception e) {
+		Log.e(TAG, "error", e);
+	    }
+	}
 
     }
 
@@ -253,52 +254,52 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      * launcherActivity} is started
      */
     private void startApp() {
-        Log.d(TAG, "startApp()");
-        try {
-            // The launcher activity set by the user as entry point is
-            // instantiated
-            launcherActivity = SGSettings.getLauncherActivity();
-            Log.d(TAG, "Starting the Activty indicated as entry point");
-            mTracker.sendView("/GameStarted");
-            Intent intent = new Intent(this, launcherActivity);
-            startActivity(intent);
-        } catch (IllegalLauncherActivityException e) {
-            Log.e(TAG, "The entry point activity is NULL");
-        } catch (Exception e) {
-            Log.e(TAG, "An error ocurred while starting the given activity.");
-        }
-        finish();
+	Log.d(TAG, "startApp()");
+	try {
+	    // The launcher activity set by the user as entry point is
+	    // instantiated
+	    launcherActivity = SGSettings.getLauncherActivity();
+	    Log.d(TAG, "Starting the Activty indicated as entry point");
+	    mTracker.sendView("/GameStarted");
+	    Intent intent = new Intent(this, launcherActivity);
+	    startActivity(intent);
+	} catch (IllegalLauncherActivityException e) {
+	    Log.e(TAG, "The entry point activity is NULL");
+	} catch (Exception e) {
+	    Log.e(TAG, "An error ocurred while starting the given activity.");
+	}
+	finish();
     }
 
     /**
      * Requests an ad and displays it during the given seconds.
      */
     private void showLoadingScreen() {
-        long adDelay = SGSettings.AD_DELAY;
-        if (!NetworkUtilities.isOnline(this)) {
-            if (SGSettings.isInternetRequired()) {
-                buildRetryConnectionDialog();
-            } else {
-                startApp();
-            }
-            
-        } else {
-            try {
-                loadingScreenAdView.loadInIframe();
-                // flipper.setInAnimation(SoftgamesUI.inFromRightAnimation());
-                flipper.setDisplayedChild(LOADING_SCREEN_ID);
-                mTracker.sendView("/LoadingScreen");
-                // Thread to show the ads during the given seconds
-                scheduleTaskExecutor.schedule(new Runnable() {
-                    @Override
-                    public void run() {
-                        startApp();
-                    }
-                }, adDelay, TimeUnit.SECONDS);
-            } catch (Exception e) {
-                Log.e(TAG, "error requesting ad", e);
-            }
-        }
+	long adDelay = SGSettings.AD_DELAY;
+	if (!NetworkUtilities.isOnline(this)) {
+	    if (SGSettings.isInternetRequired()) {
+		buildRetryConnectionDialog();
+	    } else {
+		startApp();
+	    }
+
+	} else {
+	    try {
+		loadingScreenAdView.loadInIframe();
+		// flipper.setInAnimation(SoftgamesUI.inFromRightAnimation());
+		flipper.setDisplayedChild(LOADING_SCREEN_ID);
+		mTracker.sendView("/LoadingScreen");
+		// Thread to show the ads during the given seconds
+		scheduleTaskExecutor.schedule(new Runnable() {
+		    @Override
+		    public void run() {
+			startApp();
+		    }
+		}, adDelay, TimeUnit.SECONDS);
+	    } catch (Exception e) {
+		Log.e(TAG, "error requesting ad", e);
+	    }
+	}
     }
 
     /*
@@ -308,8 +309,8 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      */
     @Override
     protected void onDestroy() {
-        scheduleTaskExecutor.shutdown();
-        super.onDestroy();
+	scheduleTaskExecutor.shutdown();
+	super.onDestroy();
     }
 
     /**
@@ -317,33 +318,34 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      * 
      */
     public void buildRetryConnectionDialog() {
-        Log.d(TAG, "buildRetryConnectionDialog()");
-        res = this.getResources();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	Log.d(TAG, "buildRetryConnectionDialog()");
+	res = this.getResources();
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setMessage(res.getString(R.string.offline_retry_msg));
-        builder.setCancelable(true);
+	builder.setMessage(res.getString(R.string.offline_retry_msg));
+	builder.setCancelable(true);
 
-        builder.setPositiveButton(res.getString(R.string.button_retry),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                        showLoadingScreen();
-                    }
-                });
+	builder.setPositiveButton(res.getString(R.string.button_retry),
+		new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int id) {
+			dialog.cancel();
+			showLoadingScreen();
+		    }
+		});
 
-        builder.setNegativeButton(res.getString(R.string.button_exit),
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                        Intent intent = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
-                        startActivityForResult(intent, ACTIVITY_RESULT_SETTINGS);
-                        //startApp();
-                    }
-                });
+	builder.setNegativeButton(res.getString(R.string.button_exit),
+		new DialogInterface.OnClickListener() {
+		    public void onClick(DialogInterface dialog, int id) {
+			dialog.cancel();
+			Intent intent = new Intent(
+				android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+			startActivityForResult(intent, ACTIVITY_RESULT_SETTINGS);
+			// startApp();
+		    }
+		});
 
-        final AlertDialog dlg = builder.create();
-        dlg.show();
+	final AlertDialog dlg = builder.create();
+	dlg.show();
     }
 
     /**
@@ -352,53 +354,54 @@ public class SoftgamesActivity extends Activity implements OnClickListener {
      * @return true, if is first session
      */
     protected boolean isFirstSession() {
-        try {
-            // Restore preferences
-            SharedPreferences sgSettings = getSharedPreferences(
-                    SGSettings.PREFS_NAME, 0);
+	try {
+	    // Restore preferences
+	    SharedPreferences sgSettings = getSharedPreferences(
+		    SGSettings.PREFS_NAME, 0);
 
-            boolean firstSession = sgSettings.getBoolean(
-                    SGSettings.FIRST_SESSION, true);
+	    boolean firstSession = sgSettings.getBoolean(
+		    SGSettings.FIRST_SESSION, true);
 
-            if (firstSession) {
-                Log.d(TAG, "This is the very first session");
-                SharedPreferences.Editor editor = sgSettings.edit();
-                editor.putBoolean(SGSettings.FIRST_SESSION, false);
-                editor.commit();
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            Log.e(TAG, "", e);
-            return true;
-        }
+	    if (firstSession) {
+		Log.d(TAG, "This is the very first session");
+		SharedPreferences.Editor editor = sgSettings.edit();
+		editor.putBoolean(SGSettings.FIRST_SESSION, false);
+		editor.commit();
+		return true;
+	    } else {
+		return false;
+	    }
+	} catch (Exception e) {
+	    Log.e(TAG, "", e);
+	    return true;
+	}
     }
 
     @Override
     protected void onStart() {
-        super.onStart();
-        
+	super.onStart();
+
     }
 
     @Override
     protected void onStop() {
-        super.onStop();
+	super.onStop();
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.button_play) {
-            showLoadingScreen();
-        }
+	if (v.getId() == R.id.button_play) {
+	    showLoadingScreen();
+	}
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 	super.onActivityResult(requestCode, resultCode, data);
 	if (requestCode == ACTIVITY_RESULT_SETTINGS) {
-	    Intent intent = new Intent(SoftgamesActivity.this, SoftgamesActivity.class);
-	        startActivity(intent);
+	    Intent intent = new Intent(SoftgamesActivity.this,
+		    SoftgamesActivity.class);
+	    startActivity(intent);
 	}
     }
 
