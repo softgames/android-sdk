dc9b735a31621b9c67e14f768fc1a5e2faa00ffb
Roland Castillo <rolandaugusto@gmail.com>
2013-06-13 13:27:54 +0200

    Custom more games button

--- src/de/softgames/sdk/ui/MoreGamesButton.java
+++ src/de/softgames/sdk/ui/MoreGamesButton.java
@@ -4,54 +4,116 @@
 package de.softgames.sdk.ui;
 
 
+import java.util.Random;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import com.google.analytics.tracking.android.GoogleAnalytics;
 import com.google.analytics.tracking.android.Tracker;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.util.AttributeSet;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 import de.softgames.sdk.R;
 
 
 /**
+ * The Custom Button MoreGamesButton.
+ *
  * @author rolandcastillo@softgames.de
- * 
  */
-// TODO extend RelativeLayout in order to add the counter?
-public class MoreGamesButton extends ImageButton implements OnClickListener {
+public class MoreGamesButton extends FrameLayout implements OnClickListener{
+    
 
+    /** The context. */
     private Context mContext;
-    private final int srcButtonId = R.drawable.sg_button_more_games_selector;
+    
+    /** The button more games. */
+    private ImageButton buttonMoreGames;
+    
+    /** The new games ticker. */
+    private TextView newGamesTicker;
+    
+    /** The Google tracker. */
     private Tracker mTracker;
+    
 
     /**
-     * @param context
+     * Instantiates a new more games button.
+     *
+     * @param context the context
      */
     public MoreGamesButton(Context context) {
         super(context);
         this.mContext = context;
-        initButton();
+        initComponents();
     }
 
     /**
-     * @param context
-     * @param attrs
+     * Instantiates a new more games button.
+     *
+     * @param context the context
+     * @param attrs the attributes
      */
     public MoreGamesButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.mContext = context;
+        initComponents();
+    }
+
+    /**
+     * Inits the components.
+     */
+    private void initComponents() {
+        initContainer();
         initButton();
+        initTicker();
     }
 
-    @Override
-    public void onClick(View v) {
-        SoftgamesUI.buildMoreGamesDialog(mContext).show();
-        trackEvent();
+    /**
+     * Inits the container.
+     */
+    private void initContainer() {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.sg_button_more_games_layout, this, true);
+        setClickable(true);
     }
 
+    /**
+     * Inits the ticker with a random number.
+     */
+    private void initTicker() {
+        newGamesTicker = (TextView) this.findViewById(R.id.sg_ticker_button_more_games);        
+        newGamesTicker.setText(getRandomNumber(1, 6) + "");
+    }
+
+    /**
+     * Inits the button.
+     */
+    private void initButton() {
+        buttonMoreGames = (ImageButton) this.findViewById(R.id.sg_button_more_games);
+        buttonMoreGames.setOnClickListener(this);
+    }
+
+    /**
+     * Gets the random number.
+     *
+     * @param low the low
+     * @param high the high
+     * @return the random number
+     */
+    private int getRandomNumber(int low, int high) {
+        Random random = new Random();        
+        return random.nextInt(high - low) + low;
+    }
+
+    /**
+     * Track event.
+     */
     private void trackEvent() {
         // The google analytics object instance
         GoogleAnalytics mInstance = GoogleAnalytics.getInstance(mContext);
@@ -60,19 +122,13 @@ public class MoreGamesButton extends ImageButton implements OnClickListener {
         mTracker.sendView("/MoreGamesScreen");
     }
 
+    /* (non-Javadoc)
+     * @see android.view.View.OnClickListener#onClick(android.view.View)
+     */
     @Override
-    public void setImageResource(int resId) {
-        super.setImageResource(resId);
-    }
-
-    private void initButton() {
-        setBackgroundDrawable(mContext.getResources().getDrawable(srcButtonId));
-        setOnClickListener(this);
-    }
-
-    @Override
-    public void setBackgroundDrawable(Drawable d) {
-        super.setBackgroundDrawable(d);
+    public void onClick(View v) {
+        SoftgamesUI.buildMoreGamesDialog(mContext).show();
+        trackEvent();
     }
 
 }
